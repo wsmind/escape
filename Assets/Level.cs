ab52d6eeedc3b13d56eeed8640361dc082afb4d0
Remi Papillie <remi.papillie@gmail.com>
2019-12-12 05:27:10 +0100

    refactored camera rig into level prefab

---
+++ Assets/Level.cs
@@ -0,0 +1,30 @@
+﻿using UnityEngine;
+
+public class Level : MonoBehaviour
+{
+    private PlayerController player;
+
+    private CameraController cameraController;
+
+    private void Awake()
+    {
+        cameraController = GetComponent<CameraController>();
+        player = GameObject.FindWithTag("Player")?.GetComponent<PlayerController>();
+
+        if (player == null)
+            Debug.LogError("No player found in this level!");
+    }
+
+    private void FixedUpdate()
+    {
+        if (player == null)
+        {
+            cameraController.TargetPosition = new Vector3(Mathf.Sin(Time.time), Mathf.Sin(Time.time * 0.7f), 0.0f);
+            return;
+        }
+
+        var playerPosition = player.gameObject.transform.position;
+
+        cameraController.TargetPosition = new Vector3(playerPosition.x, playerPosition.y, 0.0f);
+    }
+}
--- Assets/Scripts/CameraController.cs
+++ Assets/Scripts/CameraController.cs
@@ -1,29 +1,13 @@
-﻿using System.Collections;
-using System.Collections.Generic;
-using UnityEngine;
+﻿using UnityEngine;
 
 public class CameraController : MonoBehaviour
 {
-    public PlayerController player;
-    public float DampingFactor = 10.0f;
+    public Vector3 TargetPosition;
 
-    private void Awake()
-    {
-        if (player == null)
-            Debug.LogError("You need to attach the player to the camera rig!");
-    }
+    public float DampingFactor = 10.0f;
 
     private void FixedUpdate()
     {
-        if (player == null)
-        {
-            transform.position = new Vector3(Mathf.Sin(Time.time), Mathf.Sin(Time.time * 0.7f), 0.0f);
-            return;
-        }
-
-        var playerPosition = player.gameObject.transform.position;
-
-        var targetPosition = new Vector3(playerPosition.x, playerPosition.y, 0.0f);
-        transform.position = Vector3.Lerp(transform.position, targetPosition, Mathf.Min(Time.deltaTime * DampingFactor, 1.0f));
+        transform.position = Vector3.Lerp(transform.position, TargetPosition, Mathf.Min(Time.deltaTime * DampingFactor, 1.0f));
     }
 }
